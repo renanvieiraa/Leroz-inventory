67deeb91443d49508510ff689708c8f78806afe6
renanvieiraa <33770182+renanvieiraa@users.noreply.github.com>
2021-08-28 21:55:17 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow} = require('electron')
+    const url = require('url');
+    const path = require('path');
+
+    let mainWindow
+
+    function createWindow() {
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences:{
+                nodeIntegration: true
+            }
+        })
+
+        mainWindow.loadURL(
+            url.format({
+                pathname: path.join(__dirname, `/dist/investhard/index.html`),
+                protocol: "file",
+                slashes: true
+            })
+        );
+
+        // Abre a DevTools.
+        mainWindow.webContents.openDevTools()
+
+        mainWindow.on('close', ()=>{
+            mainWindow = null
+        })
+    }
+
+    app.on('ready', createWindow)
+
+    app.on('window-all-closed', ()=>{
+        if (process.platform !== 'darwin') app.quit()
+    })
+
+    app.on('activate', ()=>{
+        if(mainWindow === null) createWindow()
+    })
